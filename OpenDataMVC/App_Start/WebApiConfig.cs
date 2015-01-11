64dfaa606482cc45f8953fc02620093ca37b6d21
Ken Tseng <vul3yo6@hotmail.com>
2015-01-11 23:35:28 +0800

    Open CORS AJAX REQUEST
    Signed-off-by: Ken Tseng <vul3yo6@hotmail.com>

--- OpenDataMVC/App_Start/WebApiConfig.cs
+++ OpenDataMVC/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace OpenDataMVC
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- OpenDataMVC/Controllers/OpenApiController.cs
+++ OpenDataMVC/Controllers/OpenApiController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Xml;
 
 namespace OpenDataMvc.Controllers
